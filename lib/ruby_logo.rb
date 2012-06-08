ba28174d1a6278c214ca1b5be6ac5d251460ef06
Rich Healey <richard.healey@99designs.com>
2012-06-08 10:46:10 +1000

    Show php logo, not ruby's

--- lib/ruby_logo.rb
+++ lib/ruby_logo.rb
@@ -7,7 +7,7 @@ class RubyLogo
   def call(env)
     if env["REQUEST_URI"].include? "?=PHPE9568F34-D428-11d2-A769-00AA001ACF42"
       # Dump the ruby logo
-      [200, {"Content-Type" => "image/png"}, File.open(File.expand_path("../../assets/ruby.png", __FILE__))]
+      [200, {"Content-Type" => "image/png"}, File.open(File.expand_path("../../assets/php.gif", __FILE__))]
     else
       @app.call(env)
     end
